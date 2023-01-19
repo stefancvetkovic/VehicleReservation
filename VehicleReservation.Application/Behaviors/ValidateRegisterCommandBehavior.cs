ed13365db3fc5d1dd9fdce0e85f851568ea84e40
Stefan Cvetković <stefan_cvetkovic@outlook.com>
2023-01-19 10:28:58 +0100

    Update FluentValidations

--- VehicleReservation.Application/Behaviors/ValidateRegisterCommandBehavior.cs
+++ VehicleReservation.Application/Behaviors/ValidateRegisterCommandBehavior.cs
@@ -1,25 +1,23 @@
 ﻿using FluentValidation;
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 using VehicleReservation.Application.Exceptions;
 
 namespace VehicleReservation.Application.Behaviors
 {
-    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-        where TRequest : IRequest<TResponse>
-        where TResponse: class
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
     {
-        private readonly IEnumerable<IValidator<TRequest>> _validator;
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
 
-        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validator)
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
         {
-            _validator = validator;
+            _validators = validators;
         }
 
         public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
             var context = new ValidationContext<TRequest>(request);
-
-            var failures = _validator
+            var failures = _validators
                 .Select(v => v.Validate(context))
                 .SelectMany(result => result.Errors)
                 .Where(f => f != null)
@@ -27,7 +25,7 @@ namespace VehicleReservation.Application.Behaviors
 
             if (failures.Count != 0)
             {
-                throw new ApiException(failures);
+                throw new FluentValidation.ValidationException(failures);
             }
 
             return next();
--- VehicleReservation.Application/ServiceExtension.cs
+++ VehicleReservation.Application/ServiceExtension.cs
@@ -10,18 +10,13 @@ namespace VehicleReservation.Application
 {
     public static class ServiceExtension
     {
-        public static void AddApplicationLayer(this IServiceCollection services)
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
         {
-
-            services.AddScoped(
-                typeof(IPipelineBehavior<,>),
-                typeof(ValidationBehavior<,>));
-
-            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-             
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
+            return services;
         }
     }
 }
